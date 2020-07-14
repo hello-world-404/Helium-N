42a76dd10e01b67537f55a7018472e10ca70b533
sudo2u <kirakbit@gmail.com>
2020-07-14 09:59:33 +0800

    第一版本+部署文件

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.VisualBasic;
-using Microsoft.Win32;
 
 namespace Helium
 {
     public partial class MainWindow : Window
     {
-        private static string commandType = "/k ";
+        public static string commandType = "/k ";
+        private static string pacName;
+        private static string curDir = Directory.GetCurrentDirectory() + "\\";
         public MainWindow()
         {
             InitializeComponent();
@@ -72,7 +62,7 @@ namespace Helium
             if ((bool)of.ShowDialog())
             {
                 string pacName = of.FileName;
-                if(pacName != null)
+                if (pacName != null)
                 {
                     run("adb install " + pacName);
                 }
@@ -85,22 +75,19 @@ namespace Helium
 
         private void uninstallApk(object sender, MouseButtonEventArgs e)
         {
-            
-        }
-
-        private void pushFile(object sender, MouseButtonEventArgs e)
-        {
-            run("adb reboot");
+            run("adb uninstall " + pacName);
         }
 
         private void takeScreenshot(object sender, MouseButtonEventArgs e)
         {
-            run("adb reboot");
+            run("adb shell screencap -p /sdcard/screenshot.png");
+            System.Threading.Thread.Sleep(2000);
+            run("adb pull /sdcard/screenshot.png");
         }
 
         private void recordScreen(object sender, MouseButtonEventArgs e)
         {
-            run("adb reboot");
+            run("adb shell screenrecord /sdcard/record.mp4");
         }
 
         private void getBatteryInfo(object sender, MouseButtonEventArgs e)
@@ -110,7 +97,98 @@ namespace Helium
 
         private void run(string cm)
         {
+            checkVerbose();
             System.Diagnostics.Process.Start("cmd.exe", commandType + cm);
+        }
+
+        private void launchADB(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd.exe");
+        }
+
+        private void launchShell(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd.exe", "/k adb shell");
+        }
+
+        private void inputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                if (pacName == null)
+                {
+                    MessageBox.Show("你未输入任何东西！", "错误！", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+                else
+                {
+                    pacName = inputBox.Text;
+                }
+            }
+        }
+
+
+        private void installLoops(object sender, MouseButtonEventArgs e)
+        {
+            run("adb install " + curDir + "\\apk\\loops.apk");
+        }
+        /*
+        private void enablePillNavigation(object sender, MouseButtonEventArgs e)
+        {
+            run("adb shell getprop ro.build.version.release");
+        }
+        private void enableLoopsClock(object sender, MouseButtonEventArgs e)
+        {
+            run("adb shell getprop ro.build.version.release");
+        }
+        private void enableWhiteTheme(object sender, MouseButtonEventArgs e)
+        {
+            run("adb shell getprop ro.build.version.release");
+        }
+        */
+
+        private void launchAuthServerPage(object sender, MouseButtonEventArgs e)
+        {
+            switchAuthServerSplash sp = new switchAuthServerSplash();
+            sp.Show();
+        }
+
+        private void checkVerbose()
+        {
+            if ((bool)verbose.IsChecked)
+            {
+                commandType = "/k ";
+            }
+            else
+            {
+                commandType = "/c ";
+            }
+        }
+
+
+        private void actAirFrozen(object sender, MouseButtonEventArgs e)
+        {
+            run("adb shell dpm set-device-owner me.yourbay.airfrozen/.main.core.mgmt.MDeviceAdminReceiver");
+        }
+        private void actIceBox(object sender, MouseButtonEventArgs e)
+        {
+            run("adb shell dpm set-device-owner com.catchingnow.icebox/.receiver.DPMReceiver");
+        }
+        private void actShizuku(object sender, MouseButtonEventArgs e)
+        {
+            run("adb shell sh /data/user_de/0/moe.shizuku.privileged.api/start.sh");
+        }
+        private void actBrevent(object sender, MouseButtonEventArgs e)
+        {
+            run("adb -d shell sh /data/data/me.piebridge.brevent/brevent.sh ");
+        }
+        private void actBlackRoom(object sender, MouseButtonEventArgs e)
+        {
+            run("adb shell dpm set-device-owner web1n.stopapp/.receiver.AdminReceiver");
+        }
+
+        private void aboutMe_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/hello-world-404");
         }
     }
 }
---
+++ switchAuthServerSplash.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Helium
+{
+    public partial class switchAuthServerSplash : Window
+    {
+        private static string httpAuthServer;
+        private static string httpsAuthServer;
+        public switchAuthServerSplash()
+        {
+            InitializeComponent();
+        }
+
+        private void authXiaomi(object sender, RoutedEventArgs e)
+        {
+            httpAuthServer = "http://connect.rom.miui.com/generate_204";
+            httpsAuthServer = "https://connect.rom.miui.com/generate_204";
+            changeAuth();
+
+        }
+
+        private void authV2ex(object sender, RoutedEventArgs e)
+        {
+            httpAuthServer = "http://captive.v2ex.co/generate_204";
+            httpsAuthServer = "https://captive.v2ex.co/generate_204";
+            changeAuth();
+        }
+
+        private void changeAuth()
+        {
+            if ((bool)http.IsChecked)
+            {
+                System.Diagnostics.Process.Start("cmd.exe", MainWindow.commandType + "adb shell settings delete global captive_portal_http_url");
+                System.Diagnostics.Process.Start("cmd.exe", MainWindow.commandType + "adb shell settings put global captive_portal_http_url " + httpAuthServer);
+            }
+            else if ((bool)https.IsChecked)
+            {
+                System.Diagnostics.Process.Start("cmd.exe", MainWindow.commandType + "adb shell settings delete global captive_portal_https_url");
+                System.Diagnostics.Process.Start("cmd.exe", MainWindow.commandType + "adb shell settings put global captive_portal_https_url " + httpsAuthServer);
+            }
+            else if((bool)https.IsChecked && (bool)http.IsChecked)
+            {
+                System.Diagnostics.Process.Start("cmd.exe", MainWindow.commandType + "adb shell settings delete global captive_portal_http_url");
+                System.Diagnostics.Process.Start("cmd.exe", MainWindow.commandType + "adb shell settings put global captive_portal_http_url " + httpAuthServer);
+                System.Diagnostics.Process.Start("cmd.exe", MainWindow.commandType + "adb shell settings delete global captive_portal_https_url");
+                System.Diagnostics.Process.Start("cmd.exe", MainWindow.commandType + "adb shell settings put global captive_portal_https_url " + httpsAuthServer);
+            }
+            else
+            {
+                MessageBox.Show("未选择连接方式！", "错误！", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            
+        }
+    }
+}
